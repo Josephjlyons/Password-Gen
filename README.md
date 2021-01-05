1ab6b73874f8805fd01e084a57b9fe9f9d2b2f6a
Joe Lyons <josephjlyons90@gmail.com>
2021-01-04 23:20:18 -0500

    Final For Now

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Password-Gen
+
+## Purpose
+
+The purpose of this project was to created a random password generator that abides by certain criteria. However I was unable at this time to perfectly follow what was asked for this project and after many hours of trying to figure out how to get it right... I will have to admit defeat, only for now. I do plan to revist and polish up this project to make sure it fully works to the upmost standards. 
+
+### Codes 
+Simple HTML/CSS/JS coding was used, also the use of Bootstrap was included in this project. 
+
+### How to Use 
+Simply adjust the slide bar to select length of the password you desire from a min of 8 characters to a max of 128 characters. Once you have selected a length click generate and your password will appear in the text box. 
+
+Currently the ability to select and de-select certain characters (upper,lower,numbers,symbols) is not available. 
+
+### Mentions and Resources Used
+The teacher and TA from the UNH Coding Bootcamp.
+Many many online web services: Youtube, W3schools, MDN. 
+
+### Contact 
+josephjlyons90@gmail.com for questions or support with this program. 
--- passwordJS.js
+++ passwordJS.js
@@ -1,32 +1,20 @@
-
-
+// Function for Password Generator
 
 function generate() {
 
-   let password = "";
+    let passwordgen = document.getElementById("slider").value;
+
+    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()-_+{}\/<>"
+
+    let password = "";
 
     for (var i = 0; i <= passwordgen; i++) {
-        if (document.getElementById("lowercase").checked == true) {
-            password += String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-        } 
-        
-        if (document.getElementById("uppercase").checked == true) {
-            password += String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-        } 
-        
-        if (document.getElementById("symbol").checked == true) { 
-            password += String.fromCharCode(Math.floor(Math.random() *15) + 33);
-    
-        }
-        if (document.getElementById("number").checked == true) {
-            password += String.fromCharCode(Math.floor(Math.random() *10) + 58);
-        }
-        }
-      return password
+        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length)));
     }
 
-   
 
+    // show password
 
-
+    document.getElementById("display").value = password;
+}
 
